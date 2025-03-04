409c75b1ac1edd7ca0f9dbd2a688f0b3dea534f1
Gylmynnn <muhamadgilman999@gmail.com>
2025-03-04 22:52:02 +0800

    ref : refator config blog title

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from "$app/environment";
 
-export const title = "GylBlo9";
+export const title = "Gylmynnn :=";
 export const description = "My sveltekit static blog";
 export const url = dev ? "http://localhost:5173/" : "";
