c2456698a05f1ae0dfee7f61f43bc12e66514128
Gylmynnn <muhamadgilman999@gmail.com>
2025-03-06 18:20:23 +0800

    ref : change meta description config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from "$app/environment";
 
 export const title = "Gylmynnn :=";
-export const description = "My sveltekit static blog";
+export const description = "My Personal Website";
 export const url = dev ? "http://localhost:5173/" : "";
