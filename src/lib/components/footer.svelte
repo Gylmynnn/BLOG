3e1790916fa54fb81dd1dcd7e6365656167ec721
Gylmynnn <muhamadgilman999@gmail.com>
2025-03-06 18:20:53 +0800

    feat : add description in footer

--- src/lib/components/footer.svelte
+++ src/lib/components/footer.svelte
@@ -4,12 +4,16 @@
 
 <footer class="footer-container">
    <p>{config.title} &copy {new Date().getFullYear()}</p>
+   <p>{config.description}</p>
 </footer>
 
 <style>
    footer {
       padding-block: var(--size-7);
       border-top: 1px solid var(--size-7);
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
 
       p {
          color: var(--text-2);
