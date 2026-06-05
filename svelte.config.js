ab8157de7c71bb3cfa86579fa8cdc8f9a0fd5eaf
Gylmynnn <muhamadgilman999@gmail.com>
2026-06-05 16:55:23 +0800

    core : sql language md

--- svelte.config.js
+++ svelte.config.js
@@ -5,59 +5,60 @@ import adapter from "@sveltejs/adapter-vercel";
 import fs from "fs";
 
 const everblush = JSON.parse(
-   fs.readFileSync("./src/lib/shiki.theme.json", "utf8")
+    fs.readFileSync("./src/lib/shiki.theme.json", "utf8")
 );
 
 /** Cache highlighter agar tidak membuat instance baru setiap kali */
 let highlighterInstance = null;
 const installedLanguages = [
-   "javascript",
-   "typescript",
-   "json",
-   "go",
-   "sh",
-   "svelte",
-   "shell",
-   "python",
+    "javascript",
+    "typescript",
+    "json",
+    "go",
+    "sh",
+    "svelte",
+    "shell",
+    "python",
+    "sql"
 ];
 
 function getHighlighter() {
-   if (!highlighterInstance) {
-      highlighterInstance = createHighlighter({
-         themes: [everblush],
-         langs: installedLanguages,
-      });
-   }
-   return highlighterInstance;
+    if (!highlighterInstance) {
+        highlighterInstance = createHighlighter({
+            themes: [everblush],
+            langs: installedLanguages,
+        });
+    }
+    return highlighterInstance;
 }
 
 /** @type {import('mdsvex').MdsvexOptions} */
 const mdsvexOptions = {
-   extensions: [".md"],
-   layout: {
-      _: "./src/mdsvex.svelte",
-   },
-   highlight: {
-      highlighter: async (code, lang = "text") => {
-         const highlighter = await getHighlighter();
-         const html = escapeSvelte(
-            highlighter.codeToHtml(code, {
-               lang,
-               theme: "everblush",
-            }),
-         );
-         return `{@html \`${html}\` }`;
-      },
-   },
+    extensions: [".md"],
+    layout: {
+        _: "./src/mdsvex.svelte",
+    },
+    highlight: {
+        highlighter: async (code, lang = "text") => {
+            const highlighter = await getHighlighter();
+            const html = escapeSvelte(
+                highlighter.codeToHtml(code, {
+                    lang,
+                    theme: "everblush",
+                }),
+            );
+            return `{@html \`${html}\` }`;
+        },
+    },
 };
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-   preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
-   extensions: [".svelte", ".md"],
-   kit: {
-      adapter: adapter(),
-   },
+    preprocess: [vitePreprocess(), mdsvex(mdsvexOptions)],
+    extensions: [".svelte", ".md"],
+    kit: {
+        adapter: adapter(),
+    },
 };
 
 export default config;
